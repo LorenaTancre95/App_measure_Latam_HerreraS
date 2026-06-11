45d655187778dd0a1a4b0464e7ab6ebc901a3ad8
Silvina Herrera <silvinaherrera@latam.com>
2026-06-11 13:21:27 -0300

    revert: back to yolo11s-box (COCO detected worse)

--- boxer/YOLODetector.swift
+++ boxer/YOLODetector.swift
@@ -16,18 +16,7 @@ final class YOLODetector {
     private let session: ORTSession
     private let env: ORTEnv
 
-    static let classNames: [String] = [
-        "person","bicycle","car","motorcycle","airplane","bus","train","truck","boat",
-        "traffic light","fire hydrant","stop sign","parking meter","bench","bird","cat",
-        "dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack",
-        "umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball",
-        "kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket",
-        "bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple",
-        "sandwich","orange","broccoli","carrot","hot dog","pizza","donut","cake","chair",
-        "couch","potted plant","bed","dining table","toilet","tv","laptop","mouse",
-        "remote","keyboard","cell phone","microwave","oven","toaster","sink",
-        "refrigerator","book","clock","vase","scissors","teddy bear","hair drier","toothbrush"
-    ]
+    static let classNames: [String] = ["caja"]
 
     init(modelPath: String) throws {
         env = try ORTEnv(loggingLevel: .warning)
@@ -68,7 +57,7 @@ final class YOLODetector {
         let outputData = try outputs["output0"]!.tensorData() as Data
         let values = outputData.withUnsafeBytes { Array($0.bindMemory(to: Float.self)) }
 
-        let numClasses = 80
+        let numClasses = 1
         let numAnchors = 8400
         let stride = numAnchors
 
